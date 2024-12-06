9ca459479258edbe5e9312e0c4c81db0b1574490
Kronosthemad <167384637+Kronosthemad@users.noreply.github.com>
2024-12-05 23:50:06 -0600

    Update make-logfile.ps1

--- make-logfile.ps1
+++ make-logfile.ps1
@@ -1,45 +1,74 @@
 $L = "C:/requested log/$F"
 $S = 'C:/requested log/'
 $F = "log." + (Get-DateTime)
-beginning{
- function Get-Logfile {
-  if (Get-ChildItem -path $L is  ) {
-        pass
-  }
- }
 
- function New-Logfile {
-  try{
-   New-PSObject -file -path $S -name $F
-  }
-  catch{ 
-  }
- }
+function New-EliLog {
+  [CmdletBinding(PositionalBinding=$false,ConfirmImpact='low')]
+  [Alias(mylog)]
+  Param
+  (
+      # Param1 help description
+      [Parameter(Mandatory=$true, 
+                 ValueFromPipeline=$true,
+                 ValueFromPipelineByPropertyName=$true, 
+                 ValueFromRemainingArguments=$false, 
+                 Position=0,
+                 ParameterSetName='Parameter Set 1')]
+      [ValidateNotNull()]
+      [ValidateNotNullOrEmpty()]
+      [ValidateCount(0,5)]
+      [ValidateSet("sun", "moon", "earth")]
+      [Alias("p1")] 
+      $Param1,
 
- function New-Directory {
-  try{
-   New-PSObject -directory -path  $S
-  }
-  catch{
-  }
+      # Param2 help description
+      [Parameter(ParameterSetName='Parameter Set 1')]
+      [AllowNull()]
+      [AllowEmptyCollection()]
+      [AllowEmptyString()]
+      [ValidateScript({$true})]
+      [ValidateRange(0,5)]
+      [int]
+      $Param2,
 
- function Write-ToLog{
-  $PSItem | out-file.append -path $L
- }
+      # Param3 help description
+      [Parameter(ParameterSetName='Another Parameter Set')]
+      [ValidatePattern("[a-z]*")]
+      [ValidateLength(0,15)]
+      [String]
+      $Param3
+  )
 
- function Build-LogFile{
-  New-Directory,
-  New-LogFile
- }
-  Build-LogFile
- }
 }
+  function Get-Logfile {Get-ChildItem -path $L}
 
-foreach (PSObject in $PSItem ){
+  function New-Logfile {
+    try{
+     New-PSObject -file -path $S -name $F
+    }
+      catch{ 
+      }
+  }
+
+  function New-Directory {
+   try{
+    New-PSObject -directory $S
+   }
+   catch{
+   }
+  }
+  function Write-ToLog{
+   $PSItem | out-file.append -path $L
+  }
+
+  function Build-LogFile{
+   New-Directory
+   New-LogFile
+  }
+ foreach ($PSObject in $PSItem ){
   Get-Logfile 
   Write-ToLog 
-}
+ }
 
-end{
- Write-Out "You can find your log file at ($L)"
+Write-Out "You can find your log file at ($L)"
 }
